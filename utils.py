f6452e977444d86e749c4e1b4db5d22061143f10
adeelmurtaza0700-cell <adeelmurtaza0700@gmail.com>
2025-11-25 23:04:23 -0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,17 +3,9 @@ from reportlab.pdfgen import canvas
 def generate_certificate(name, quiz_title, score, file_path):
     c = canvas.Canvas(file_path)
     c.setFont("Helvetica-Bold", 24)
-    c.drawCentredString(300, 500, f"Certificate of Completion")
+    c.drawCentredString(300, 500, "Certificate of Completion")
     c.setFont("Helvetica", 18)
     c.drawCentredString(300, 450, f"Presented to: {name}")
     c.drawCentredString(300, 400, f"For completing the quiz: {quiz_title}")
     c.drawCentredString(300, 350, f"Score: {score}")
     c.save()
-    document.addEventListener('contextmenu', event => event.preventDefault());
-document.addEventListener('copy', event => event.preventDefault());
-document.addEventListener('keydown', event => {
-    if (event.key === "F12" || (event.ctrlKey && event.shiftKey && event.key === "I")) {
-        event.preventDefault();
-    }
-});
-window.onblur = function() {alert('Do not switch tabs!');};
