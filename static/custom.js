161388506ee2abe5a5eac65b8adfe6af7b644485
adeelmurtaza0700-cell <adeelmurtaza0700@gmail.com>
2025-11-25 23:12:17 -0800

    Delete static directory

--- static/custom.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('contextmenu', event => event.preventDefault());
-document.addEventListener('copy', event => event.preventDefault());
-document.addEventListener('keydown', event => {
-    if(event.key==="F12" || (event.ctrlKey && event.shiftKey && event.key==="I")){
-        event.preventDefault();
-    }
-});
-window.onblur = function(){alert("Do not switch tabs!");};
